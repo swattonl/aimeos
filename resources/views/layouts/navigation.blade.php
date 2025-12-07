584715890cc9f1b8163f208f4d00bb1ebde3dd95
swattonl <84343692+swattonl@users.noreply.github.com>
2025-12-07 10:35:48 +0000

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ airoute('dashboard') }}">
-                        <x-application-logo class="block w-250px fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block h-9 w-200px fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
